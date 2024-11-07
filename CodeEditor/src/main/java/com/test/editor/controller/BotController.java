7bb65c78c6770de6c857adcaec344c9ef32d2cd1
JayChoi97 <chol6245@naver.com>
2024-11-07 09:13:32 +0900

    .

---
+++ CodeEditor/src/main/java/com/test/editor/controller/BotController.java
@@ -0,0 +1,5 @@
+package com.test.editor.controller;
+
+public class BotController {
+
+}
--- CodeEditor/src/main/java/com/test/editor/controller/MainController.java
+++ CodeEditor/src/main/java/com/test/editor/controller/MainController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
