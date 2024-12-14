cc38caeb621ca930d97b51ac75840f70307bde92
chimy2 <premet@naver.com>
2024-12-15 03:56:45 +0900

    Add transaction settings

--- CodeEditor/src/main/java/com/test/editor/databind/AppConfig.java
+++ CodeEditor/src/main/java/com/test/editor/databind/AppConfig.java
@@ -2,10 +2,12 @@ package com.test.editor.databind;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
+@EnableTransactionManagement(proxyTargetClass = true)
 public class AppConfig {
 
     @Bean
